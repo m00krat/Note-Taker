68f56c0f0715c7c3622d5caf0a2a0c6c7b40966b
m00krat <prevostm17@yahoo.com>
2023-06-04 15:23:47 -0400

    finished server and created test but still no work

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -10,27 +10,37 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const getNotes = () => {
-    const data = fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf8');
-    return JSON.parse(data);
+  const data = fs.readFileSync(path.join(__dirname, 'public', 'assets', 'db', 'db.json'), 'utf8');
+  return JSON.parse(data);
 };
 
-const saveNotes = (note) => {
-    const notes = getNotes();
+const saveNotes = (newNote) => {
+  const notes = getNotes();
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'public', 'assets', 'db', 'db.json'), JSON.stringify(notes));
 };
 
-//get routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-  
-  
+
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
-  
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  saveNotes(newNote);
+  res.sendStatus(200);
+});
+
+app.get('/api/notes', (req, res) => {
+  const notes = getNotes();
+  res.json(notes);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
-  
+
 module.exports = app;
-  
