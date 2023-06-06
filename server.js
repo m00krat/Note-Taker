a748923ad19a26cee056ef41d2a243512b9fa8aa
m00krat <prevostm17@yahoo.com>
2023-06-05 21:03:38 -0400

    corrected path.join in server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const getNotes = () => {
   const data = fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'utf8');
